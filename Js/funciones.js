9c858f6b038210f101d7a7d8033156c85d2c8292
Javitocatral <javitocatral@gmail.com>
2022-02-23 11:54:52 +0100

    animación
    animación

--- Js/funciones.js
+++ Js/funciones.js
@@ -6,36 +6,87 @@ $(".amarillo").click(function(){
 console.log("clic cuadrado");
 
    $(".amarillo").toggleClass("grande");
-   // $(this).removeClass("grande");
+  $(".rosa").removeClass("grande");
+  $(".azul").removeClass("grande");
    $(".yo").toggleClass("abierto");
-   // $(this).removeClass("abierto");
-   $(".amarrillo").toggleClass("oculto");
-   // $(this).removeClass("oculto");
+ $(".cv").removeClass("abierto")
+ $(".proyectos").removeClass("abierto")
+   
+   $(".rosa").toggleClass("oculto");
+   $(".azul").toggleClass("oculto");
+   
   
 });
 $(".rosa").click(function(){
   
-   
    $(".rosa").toggleClass("grande");
-   // $(this).removeClass("grande");
+   $(".amarillo").removeClass("grande");
+   $(".azul").removeClass("grande");
       $(".cv").toggleClass("abierto");
-      // $(this).removeClass("abierto");
-      $(this).addClass("oculto");
-      $(".rosa").removeClass("oculto");
+     $(".yo").removeClass("abierto");
+     $(".proyectos").removeClass("abierto");
+
+      $(".amarillo").toggleClass("oculto");
+      $(".azul").toggleClass("oculto");
+      
       
       
    });
    
    $(".azul").click(function(){
      
-      
-      
       $(".azul").toggleClass("grande");
-      // $(this).removeClass("grande");
-         $(".proyectos").toggleClass("abierto");
-         $(this).removeClass("abierto");
+      $(".rosa").removeClass("grande");
+   $(".amarillo").removeClass("grande");
+       $(".proyectos").toggleClass("abierto");
+       $(".yo").removeClass("abierto");
+       $(".cv").removeClass("abierto");
+       $(".rosa").toggleClass("oculto");
+       $(".amarillo").toggleClass("oculto");
+        
         
       });
+      // cuando hago clic en .amarillo necesito la clase grande, la clase abierto para .yo  y la clase oculto para .rosa y .azul y que no tenga la clase oculto tambien necesito que .rosa y .azul no tengan la clase grande
+      $(".botonamarillo").click(function(){
+         console.log("clic botonamarillo")
+         $(".amarillo").addClass("grande");
+         $(".yo").addClass("abierto");
+         $(".cv").removeClass("abierto");
+         $(".proyectos").removeClass("abierto")
+         $(".rosa").addClass("oculto");
+         $(".azul").addClass("oculto");
+         $(".amarillo").removeClass("oculto");
+         $(".rosa").removeClass("grande");
+         $(".azul").removeClass("grande");
+
+      })
+      $(".botonrosa").click(function(){
+         console.log("clic botonamarillo")
+         $(".rosa").addClass("grande");
+         $(".cv").addClass("abierto");
+         $(".yo").removeClass("abierto");
+         $(".proyectos").removeClass("abierto")
+         $(".amarillo").addClass("oculto");
+         $(".azul").addClass("oculto");
+         $(".rosa").removeClass("oculto");
+         $(".amarillo").removeClass("grande");
+         $(".azul").removeClass("grande");
+
+      })
+      $(".botonazul").click(function(){
+         console.log("clic botonamarillo")
+         $(".azul").addClass("grande");
+         $(".proyectos").addClass("abierto");
+         $(".cv").removeClass("abierto");
+         $(".yo").removeClass("abierto")
+         $(".rosa").addClass("oculto");
+         $(".amarillo").addClass("oculto");
+         $(".azul").removeClass("oculto");
+         $(".rosa").removeClass("grande");
+         $(".amarillo").removeClass("grande");
+
+      })
+      
  
 
 
